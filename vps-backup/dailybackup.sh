1b3c1115dd761577291e46efd619789ea8d78785
Duncan Bennie <duncan@duncanbennie.com>
2022-06-13 23:54:10 +1000

    change provider to ovh

--- vps-backup/dailybackup.sh
+++ vps-backup/dailybackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tar -zcvf /home/sunday/dailybackup`date +%Y%m%d`.tar.gz /home/sunday
-rclone copy /home/sunday/dailybackup`date +%Y%m%d`.tar.gz onedrive:daily -PL
+rclone copy /home/sunday/dailybackup`date +%Y%m%d`.tar.gz ovh:daily
 rm /home/sunday/dailybackup`date +%Y%m%d`.tar.gz
 
-rclone delete --min-age 30d onedrive:daily
+rclone delete --min-age 15d ovh:daily
