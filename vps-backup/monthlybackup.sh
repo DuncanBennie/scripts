b49b259edaca91f6970ff76d2cbf8ae2052f7704
Duncan Bennie <duncan@duncanbennie.com>
2022-06-14 00:55:15 +1000

    fix typo

--- vps-backup/monthlybackup.sh
+++ vps-backup/monthlybackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tar -zcvf /home/sunday/monthlybackup`date +%Y%m%d`.tar.gz /home/sunday/.docker
-rclone copy /home/sunday/montlybackup`date +%Y%m%d`.tar.gz ovh:monthly -P
+rclone copy /home/sunday/monthlybackup`date +%Y%m%d`.tar.gz ovh:monthly -P
 rm /home/sunday/monthlybackup`date +%Y%m%d`.tar.gz
 
 rclone delete --min-age 1y ovh:monthly
